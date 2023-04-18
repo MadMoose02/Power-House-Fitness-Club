1efaa4fa07de7e412337b9056ddc8ea1c9158d6a
Keshan Moosai <keshanmoosai@gmail.com>
2023-04-18 19:28:19 -0400

    Added all required view controllers

--- App/views/index.py
+++ App/views/index.py
@@ -8,12 +8,14 @@ index_views = Blueprint('index_views', __name__, template_folder='../templates')
 def index_page():
     return render_template('index.html')
 
+
 @index_views.route('/init', methods=['GET'])
 def init():
     db.drop_all()
     db.create_all()
     create_user('bob', 'bobpass')
     return jsonify(message='db initialized!')
+
 
 @index_views.route('/healthcheck', methods=['GET'])
 def health_check():
