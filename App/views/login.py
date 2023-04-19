f166a70e11b5f726a14a22514ee73d3fa64d9e2a
Keshan Moosai <keshanmoosai@gmail.com>
2023-04-19 16:35:17 -0400

    Removed duplicate /login view

--- App/views/login.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify
-from App.models import db
-
-login_views = Blueprint('login_views', __name__, template_folder='../templates')
-
-@login_views.route('/login', methods=['GET'])
-def login_page():
-    return render_template('login.html')
