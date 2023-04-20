ecd1da5080c5563e1d9a3b24113035c3471e37a5
Keshan Moosai <keshanmoosai@gmail.com>
2023-04-20 18:17:11 -0400

    Added mobile nav and functionality

--- App/static/main.js
+++ App/static/main.js
@@ -1,13 +1,19 @@
-const slideModalDown = [
+const slideNavProfileModalDown = [
     { transform: 'translateY(-100%)' },
     { transform: 'translateY(0)' }
 ];
-
-const slideModalUp = [
+const slideNavProfileModalUp = [
     { transform: 'translateY(0)' },
     { transform: 'translateY(-100%)' }
 ];
-
+const slideSidePanelIntoView = [
+    { transform: 'translateX(100%)' },
+    { transform: 'translateX(0%)' }
+];
+const slideSidePanelOutOfView = [
+    { transform: 'translateX(0%)' },
+    { transform: 'translateX(100%)' }
+]
 const animationDelay = 550;
 const slideTiming = { duration: animationDelay, easing: 'ease-in-out'};
 
@@ -23,10 +29,13 @@ document.addEventListener('DOMContentLoaded', function () {
     let userNavProfile = document.querySelector('div#user-nav-profile');
     let userNavProfileModal = document.getElementById('user-nav-profile-modal');
 
+    // Reset hamburger menu icon to default state
+    document.querySelector('#nav-menu-checkbox').checked = false;
+
     // Open user profile modal when icon in navbar is clicked 
     userNavProfile.onclick = function () {
         userNavProfileModal.style.display = 'block';
-        userNavProfileModal.animate(slideModalDown, slideTiming);
+        userNavProfileModal.animate(slideNavProfileModalDown, slideTiming);
         userNavProfile.firstChild.style.filter = 'brightness(70%)';
         return;
     }
@@ -39,23 +48,43 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // If the user clicks anywhere outside of the modal (denoted by bounding box), close it
 window.onclick = function (event) {
+    
+    let sidePanelMenu = document.querySelector('#side-panel-menu');
     let userNavProfile = document.querySelector('div#user-nav-profile');
-    if (event.target.id === 'user-nav-profile-img') return;
-    if (event.target.id === 'register-sign-in-btn') return;
     let userNavProfileModal = document.getElementById('user-nav-profile-modal');
+    let sidePanelMenuBoundingBox = sidePanelMenu.getBoundingClientRect();
     let navProfileModalBoundingBox = userNavProfileModal.getBoundingClientRect();
-
     let x = event.clientX, y = event.clientY;
+    
+    if (event.target.id === 'user-nav-profile-img' || event.target.parentNode.id === 'user-nav-profile') return;
+    if (event.target.id === 'register-sign-in-btn') return;
+
     console.info(`Conditions to close: \
     \n${x} < ${navProfileModalBoundingBox.left}: ${x < navProfileModalBoundingBox.left}\
     \n${y} > ${navProfileModalBoundingBox.bottom}: ${y > navProfileModalBoundingBox.bottom}`);
     
     if (x < navProfileModalBoundingBox.left || y > navProfileModalBoundingBox.bottom) {
-        userNavProfileModal.animate(slideModalUp, slideTiming);
+        userNavProfileModal.animate(slideNavProfileModalUp, slideTiming);
         setTimeout(function () {
             userNavProfileModal.style.display = 'none';
         }, animationDelay);
         userNavProfile.firstChild.style.filter = 'brightness(100%)';
         return;
+    }
+}
+
+function toggleNavMobileMenu() {
+    console.log("Hamburger menu toggled");
+    let sidePanelMenu = document.querySelector('#side-panel-menu')
+
+    if (!sidePanelMenu.classList.contains('hidden')) {
+        sidePanelMenu.animate(slideSidePanelOutOfView, slideTiming);
+        setTimeout(function() {
+            sidePanelMenu.classList.add('hidden');
+        }, animationDelay);
+        
+    } else {
+        sidePanelMenu.classList.remove('hidden');
+        sidePanelMenu.animate(slideSidePanelIntoView, slideTiming);
     }
 }
