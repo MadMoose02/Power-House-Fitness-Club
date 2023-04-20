905d8aa552294a1c4b82b81461b3e0dc9d98f2fe
Keshan Moosai <keshanmoosai@gmail.com>
2023-04-20 12:56:56 -0400

    Fetch packages for rendering

--- App/views/packages.py
+++ App/views/packages.py
@@ -2,11 +2,11 @@ from flask import Blueprint, redirect, render_template, request, send_from_direc
 from flask_login import current_user
 
 from App.models import db
-from App.controllers import retrieve_current_user
+from App.controllers import retrieve_current_user, get_packages
 
 packages_views = Blueprint('packages_views', __name__, template_folder='../templates')
 
 @packages_views.route('/packages', methods=['GET'])
 def packages_page():
     user = retrieve_current_user() if current_user.is_authenticated else None
-    return render_template('packages.html', user=user)
+    return render_template('packages.html', user=user, packages=get_packages())
