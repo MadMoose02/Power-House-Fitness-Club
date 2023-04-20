0e593160ad84aed76060609474773f2c9968b120
Keshan Moosai <keshanmoosai@gmail.com>
2023-04-20 12:30:04 -0400

    Retreive package data for rendering

--- App/views/register.py
+++ App/views/register.py
@@ -2,11 +2,11 @@ from flask import Blueprint, redirect, render_template, request, send_from_direc
 from flask_login import current_user
 
 from App.models import db
-from App.controllers import retrieve_current_user
+from App.controllers import retrieve_current_user, get_packages
 
 register_views = Blueprint('register_views', __name__, template_folder='../templates')
 
 @register_views.route('/register', methods=['GET'])
 def register_page():
     user = retrieve_current_user() if current_user.is_authenticated else None
-    return render_template('register.html', user=user)
+    return render_template('register.html', user=user, packages=get_packages())
