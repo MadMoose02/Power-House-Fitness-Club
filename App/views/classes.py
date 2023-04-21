9c1b508770770f6a58ea472a2b2ab1cf97e8378e
Keshan Moosai <keshanmoosai@gmail.com>
2023-04-20 21:20:05 -0400

    Retreive class object for rendering with jinja

--- App/views/classes.py
+++ App/views/classes.py
@@ -2,11 +2,11 @@ from flask import Blueprint, redirect, render_template, request, send_from_direc
 from flask_login import current_user
 
 from App.models import db
-from App.controllers import retrieve_current_user
+from App.controllers import retrieve_current_user, get_classes
 
 classes_views = Blueprint('classes_views', __name__, template_folder='../templates')
 
 @classes_views.route('/classes', methods=['GET'])
 def classes_page():
     user = retrieve_current_user() if current_user.is_authenticated else None
-    return render_template('classes.html', user=user)
+    return render_template('classes.html', user=user, classes=get_classes())
