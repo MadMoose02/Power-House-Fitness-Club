726f6be8f08bff04b433c1a94b030f92967475d7
Keshan Moosai <keshanmoosai@gmail.com>
2023-04-21 21:57:09 -0400

    Added get_class()

--- App/controllers/classes.py
+++ App/controllers/classes.py
@@ -17,5 +17,9 @@ def create_classes(classes: dict) -> None:
         create_class(class_['name'], class_['instructor'], class_['description'])
 
 
+def get_class(id) -> Class:
+    return Class.query.get(id)
+
+
 def get_classes() -> list[Class]:
     return Class.query.all()
