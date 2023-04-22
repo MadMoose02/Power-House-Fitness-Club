e1e64cd27a8db845acae5dff2b71ca0d03752d0e
Keshan Moosai <keshanmoosai@gmail.com>
2023-04-21 21:13:37 -0400

    Secured endpoint & data format issue

--- App/views/profile.py
+++ App/views/profile.py
@@ -1,12 +1,17 @@
 from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify
-from flask_login import current_user
+from flask_login import current_user, login_required
 
 from App.models import db
-from App.controllers import retrieve_current_user
+from App.controllers import get_packages, get_emergency_contact
 
 profile_views = Blueprint('profile_views', __name__, template_folder='../templates')
 
 @profile_views.route('/profile', methods=['GET'])
+@login_required
 def profile_page():
-    user = retrieve_current_user() if current_user.is_authenticated else None
-    return render_template('profile.html', user=user)
+    return render_template(
+        'profile.html', 
+        user=current_user, 
+        packages=get_packages(), 
+        emergency_contact=get_emergency_contact(current_user.emergency_contact_id).get_json()
+    )
