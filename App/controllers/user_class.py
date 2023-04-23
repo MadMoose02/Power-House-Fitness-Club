4ea81e07a9847ff2f07b2a90c5df795e11bdd00f
Keshan Moosai <keshanmoosai@gmail.com>
2023-04-23 18:20:43 -0400

    Refactored code

--- App/controllers/user_class.py
+++ App/controllers/user_class.py
@@ -1,19 +1,20 @@
 from App.models import UserClass
 from App.database import db
 
-def create_userclass(user_id, class_id, class_name) -> UserClass:
+def create_userclass(user_id, class_id, class_name, paid_by_fitcoin) -> UserClass:
     new_userclass = UserClass(
         user_id=user_id,
         class_id=class_id,
-        class_name=class_name
+        class_name=class_name,
+        paid_by_fitcoin=paid_by_fitcoin
     )
     db.session.add(new_userclass)
     db.session.commit()
     return new_userclass
 
 
-def get_userclass(id) -> UserClass:
-    return UserClass.query.get(id)
+def get_userclass(user_id, class_id) -> UserClass:
+    return UserClass.query.filter_by(user_id=user_id, class_id=class_id).first()
 
 
 def get_userclasses_by_user_id(user_id) -> list[UserClass]:
@@ -29,6 +30,5 @@ def get_all_userclasses_json() -> list[dict]:
 
 
 def delete_userclass(user_id, class_id) -> None:
-    userclass = UserClass.query.filter_by(user_id=user_id, class_id=class_id).first()
-    db.session.delete(userclass)
+    db.session.delete(get_userclass(user_id, class_id))
     db.session.commit()
