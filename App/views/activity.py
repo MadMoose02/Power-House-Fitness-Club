8d357b57509cd780edbac469284d710b5040daaf
Keshan Moosai <keshanmoosai@gmail.com>
2023-04-22 21:18:07 -0400

    Fully functioning activity logger

--- App/views/activity.py
+++ App/views/activity.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, redirect, render_template, request, url_for
+from datetime import date
+from flask import Blueprint, redirect, render_template, request, url_for, flash
 from flask_login import current_user, login_required
 
 from App.models import db
-from App.controllers import retrieve_current_user, get_package
+from App.controllers import retrieve_current_user, get_package, create_activity
 
 activity_views = Blueprint('activity_views', __name__, template_folder='../templates')
 
@@ -18,6 +19,21 @@ def log_activity_page():
 @login_required
 def add_activity_log():
     print(request.form.to_dict())
+    if create_activity(
+        user_id=current_user.id,
+        date=date(
+            year=int(request.form['date'].split('-')[0]), 
+            month=int(request.form['date'].split('-')[1]), 
+            day=int(request.form['date'].split('-')[2])
+        ),
+        pre_workout=bool(request.form['pre-workout']),
+        energy_level=request.form['energy-level'],
+        details=request.form['details'],
+    ):
+        flash("Activity successfully logged", category='success')
+    else:
+        flash("Something went wrong whilst logging your activity. Try again", category='error')
+        db.rollback()
     return redirect(url_for('activity_views.log_activity_page'))
 
 
