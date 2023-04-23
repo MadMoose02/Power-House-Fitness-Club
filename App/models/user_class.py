e88d9ecbd553a65705a06a81af9ed643a985bfbc
Keshan Moosai <keshanmoosai@gmail.com>
2023-04-23 18:20:59 -0400

    Added paid_by_fitcoin property

--- App/models/user_class.py
+++ App/models/user_class.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.orm import relationship
 from App.database import db
 
@@ -10,16 +10,19 @@ class UserClass(db.Model):
     user_id = Column(Integer, db.ForeignKey('user.id'), nullable=False)
     class_id = Column(Integer, db.ForeignKey('classes.id'), nullable=False)
     class_name = Column(String(20), name="name", nullable=False, unique=False)
+    paid_by_fitcoin = Column(Boolean, name="paid_by_fitcoin", nullable=False, unique=False)
 
-    def __init__(self, user_id, class_id, class_name) -> dict:
+    def __init__(self, user_id, class_id, class_name, paid_by_fitcoin):
         self.user_id = user_id
         self.class_id = class_id
         self.class_name = class_name
+        self.paid_by_fitcoin = paid_by_fitcoin
         
     def get_json(self) -> dict:
         return {
             'id': self.id,
             'user_id': self.user_id,
             'class_id': self.class_id,
-            'class_name': self.class_name
+            'class_name': self.class_name,
+            'paid_by_fitcoin': self.paid_by_fitcoin
         }
