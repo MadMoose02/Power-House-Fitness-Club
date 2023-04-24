c9a96109d27643f5b02c2f09272fc639dea0eebb
Josiah <jojotenia@gmail.com>
2023-04-24 03:42:24 -0400

    updated wallet

--- App/views/wallet.py
+++ App/views/wallet.py
@@ -1,17 +1,17 @@
 from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify
 from flask_login import current_user, login_required
 
-from App.models import db,Wallet
-from App.controllers import retrieve_current_user, get_package
+from App.models import db, Wallet
+from App.controllers import retrieve_current_user, get_package, get_all_transactions_of_user_json
 
 wallet_views = Blueprint('wallet_views', __name__, template_folder='../templates')
 
 @wallet_views.route('/wallet', methods=['GET'])
 @login_required
 def wallet_page():
-    user = retrieve_current_user() if current_user.is_authenticated else None
+    user = current_user
     user_package = get_package(user.package_id).get_json() if user else None
-
     wallet = Wallet.query.filter_by(id=user.wallet_id).first()
+    transactions = get_all_transactions_of_user_json(user.id)
 
-    return render_template('wallet.html', user=user, user_package=user_package, wallet=wallet)
+    return render_template('wallet.html', user=user, user_package=user_package, wallet=wallet, transactions=transactions)
