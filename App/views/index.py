ca55d49e4809af03be96966d3258b37a36cf2e04
Keshan Moosai <keshanmoosai@gmail.com>
2023-04-24 02:00:01 -0400

    Added photos and buttons to respective containers

--- App/views/index.py
+++ App/views/index.py
@@ -2,7 +2,13 @@ from flask import Blueprint, render_template, jsonify
 from flask_login import current_user
 
 from App.models import db
-from App.controllers import retrieve_current_user, get_package
+from App.controllers import (
+    retrieve_current_user, 
+    get_package, 
+    get_all_classes_json,
+    get_all_packages_json,
+    get_all_facilities_json
+)
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
 
@@ -10,7 +16,15 @@ index_views = Blueprint('index_views', __name__, template_folder='../templates')
 def home_page():
     user = retrieve_current_user() if current_user.is_authenticated else None
     user_package = get_package(user.package_id).get_json() if user else None
-    return render_template('index.html', user=user, user_package=user_package)
+    return render_template(
+        'index.html', 
+        user=user, 
+        user_package=user_package, 
+        classes=get_all_classes_json(),
+        packages=get_all_packages_json(),
+        facilities=get_all_facilities_json(),
+        len=len
+    )
 
 
 @index_views.route('/healthcheck', methods=['GET'])
